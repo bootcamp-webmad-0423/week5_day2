1d9c4ec2ba00a245e439e9f6054a8975be4f304b
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2023-05-10 15:56:53 +0200

    Initial commit

---
+++ characters-crud/services/characters-api.service.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+
+class CharactersApiHandler {
+
+    constructor() {
+        this.axiosApp = axios.create({
+            baseURL: 'https://ih-crud-api.herokuapp.com'
+        })
+    }
+
+    getAllCharacters() {
+        return this.axiosApp.get('/characters')
+    }
+
+    getOneCharacter(characterId) {
+        return this.axiosApp.get(`/characters/${characterId}`)
+    }
+
+    saveCharacter(characterInfo) {
+        return this.axiosApp.post(`/characters`, characterInfo)
+    }
+
+    editCharacter(characterId, characterInfo) {
+        return this.axiosApp.put(`/characters/${characterId}`, characterInfo)
+    }
+}
+
+const charactersApiHandler = new CharactersApiHandler()
+
+module.exports = charactersApiHandler
